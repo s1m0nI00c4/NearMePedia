56e6083a9c3da3464592aba07ad7b8027a0668a0
s1m0nI00c4 <43318934+s1m0nI00c4@users.noreply.github.com>
2019-12-01 10:19:49 +0100

    Basic navigation

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator} from 'react-navigation-stack'
+import {createSwitchNavigator, createAppContainer } from 'react-navigation'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const ScreenB = props => (
+    <View>
+      <Button title="Go to ScreenA" onPress={() => props.navigation.navigate("RouteA")} />
+      <Button title="back" onPress={() => props.navigation.goBack()} />
+    </View>)
+
+ScreenB.navigationOptions = {title: "Screen B"}
+
+const ScreenA = props => (
+    <View style="styles.container">
+      <Button title="Go to ScreenB" onPress={() => props.navigation.navigate("RouteB")} />
+      <Button title="Go to ScreenC (ReadingList)" onPress={() => props.navigation.navigate("RouteC")} />
+      <Button title="back" onPress={() => props.navigation.goBack()} />
+    </View>)
+
+ScreenA.navigationOptions = {title: "Screen A"}
+
+const ScreenC = props => (
+    <View>
+      <Button title="Go to ScreenB" onPress={() => props.navigation.navigate("RouteB")} />
+      <Button title="back" onPress={() => props.navigation.goBack()} />
+    </View>)
+
+ScreenC.navigationOptions = {title: "Screen C"}
+
+
+const routes = {
+    RouteA: {screen: ScreenA},
+    RouteB: {screen: ScreenB},
+    RouteC: {screen: ScreenC}
 }
+
+const options = {initialRouteName: 'RouteB' }
+const AppNavigator = createStackNavigator(routes, options)
+
+export default createAppContainer(AppNavigator)
+
 
 const styles = StyleSheet.create({
   container: {

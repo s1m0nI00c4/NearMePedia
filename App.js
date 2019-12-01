e69f9e0d4ffdbc95b8bb058ffcae5c23d473d1b2
s1m0nI00c4 <43318934+s1m0nI00c4@users.noreply.github.com>
2019-12-01 11:59:07 +0100

    Fetching data from Wikipedia

--- App.js
+++ App.js
@@ -11,14 +11,67 @@ const ScreenB = props => (
 
 ScreenB.navigationOptions = {title: "Screen B"}
 
-const ScreenA = props => (
+const ScreenA = async props =>{
+
+  var url = "https://en.wikipedia.org/w/api.php";
+
+  var params = {
+    action: "query",
+    list: "geosearch",
+    gscoord: "37.7891838|-122.4033522",
+    gsradius: "10000",
+    gslimit: "10",
+    format: "json"
+  };
+
+  url = url + "?origin=*";
+  Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+
+  const response = await fetch(url);
+  var myJSONResponse = await response.json();
+  console.log(myJSONResponse)
+  var placesArray = await myJSONResponse.query.geosearch;
+  for (var place in placesArray) {
+            console.log(placesArray[place].title);
+        }
+
+  return(
     <View>
       <Button title="Go to ScreenB" onPress={() => props.navigation.navigate("RouteB")} />
       <Button title="Go to ScreenC (ReadingList)" onPress={() => props.navigation.navigate("RouteC")} />
       <Button title="back" onPress={() => props.navigation.goBack()} />
+      {placesArray.map(place => (<Text>{place.title}</Text>) )}
     </View>)
+}
 
 ScreenA.navigationOptions = {title: "Screen A"}
+
+const ComponentA = props => {
+  var url = "https://en.wikipedia.org/w/api.php";
+
+  var params = {
+    action: "query",
+    list: "geosearch",
+    gscoord: "37.7891838|-122.4033522",
+    gsradius: "10000",
+    gslimit: "10",
+    format: "json"
+  };
+
+  url = url + "?origin=*";
+  Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+
+  var pages = [];
+
+  fetch(url)
+    .then(function(response){return response.json();})
+    .then(function(response) {
+        pages = response.query.geosearch;
+    })
+    .catch(function(error){console.log(error);});
+
+    return (pages.map(place => {<Text>{place.title}</Text>}))
+}
 
 const ScreenC = props => (
     <View style="style.container">
